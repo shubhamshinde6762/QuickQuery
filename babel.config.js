f92ad40e1ba0fd41739449e9b383a5ef6999c85e
shubhamshinde6762 <shubhamshinde6762@gmail.com>
2024-06-11 13:08:15 +0530

    setup done

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env', // Enable modern JavaScript features
+      '@babel/preset-react' // Enable JSX syntax transformation
+    ]
+  };
+  
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/manifest.json", to: "manifest.json" }],
+    }),
+  ],
+};
