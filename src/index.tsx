72aef5222fd1b86f73c28701c2ca28ed36d2b50b
shubhamshinde6762 <shubhamshinde6762@gmail.com>
2024-06-11 18:43:08 +0530

    stash

---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+const index = <h1>hi</h1>;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  output: {
+    filename: "index.js",
+  },
+};
